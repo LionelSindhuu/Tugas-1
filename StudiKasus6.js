671e574dedfb8549f7301ec1898b332dc83f38f6
Lionel <minjuwork2@gmail.com>
2026-07-20 15:40:02 +0700

    studikasus6

---
+++ StudiKasus6.js
@@ -0,0 +1,12 @@
+const umur = [8, 13, 17, 20, 25, 11];
+
+for (let i = 0; i < umur.length; i++) {
+  
+  if (umur[i] >= 17) {
+    status = "Boleh masuk";
+  } else {
+    status = "Tidak boleh masuk";
+  }
+  
+  console.log(`Siswa ke-${i + 1} : ${status}`);
+}
