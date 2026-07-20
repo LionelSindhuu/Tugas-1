880b369590f14ef65dee52d50be9eeb5fe676933
Lionel <minjuwork2@gmail.com>
2026-07-21 06:01:59 +0700

    studikasus10

---
+++ StudiKasus10.js
@@ -0,0 +1,17 @@
+const skor = [100, 450, 800, 150, 900];
+
+for (let i = 0; i < skor.length; i++) {
+  let rank = "";
+
+  if (skor[i] >= 800) {
+    rank = "Mythic";
+  } else if (skor[i] >= 500) {
+    rank = "Legend";
+  } else if (skor[i] >= 300) {
+    rank = "Epic";
+  } else {
+    rank = "Warrior";
+  }
+
+  console.log(`Player ${i + 1} : ${skor[i]} -> ${rank}`);
+}
